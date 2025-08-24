542c844b7cb7a43edd7ab883f1fbb73fc032e67a
Sam-Zelcer <samzelcerr@gmail.com>
2025-08-24 12:44:28 +0200

    initial commit

---
+++ src/main/java/com/crl/hh/CrlApplication.java
@@ -0,0 +1,13 @@
+package com.crl.hh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crl/hh/CrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crl.hh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
